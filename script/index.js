679c08278eb00a1a72789eba13ae1a08741160e2
Jahidulislam23 <jahidul110j@gmail.com>
2025-03-13 00:34:23 +0600

    adeede the all file done

--- script/index.js
+++ script/index.js
@@ -1,5 +1,23 @@
 console.log("index is connected");
 
+const showLoader = ()=>{
+    document.getElementById("loader").classList.remove("hidden");
+    document.getElementById("video-container").classList.add("hidden");
+}
+
+const hideLoader = ()=>{
+    document.getElementById("loader").classList.add("hidden");
+    document.getElementById("video-container").classList.remove("hidden");
+}
+
+function removeActiveClass(){
+    const activeButton=document.getElementsByClassName("active");
+    for(let btn of activeButton){
+        btn.classList.remove("active");
+    }
+}
+
+
 function loadCategories(){
     //1-fetch the data
 fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
@@ -10,23 +28,66 @@ fetch("https://openapi.programming-hero.com/api/phero-tube/categories")
 
 }
 
-function loadVideos(){
-    fetch("https://openapi.programming-hero.com/api/phero-tube/videos")
+function loadVideos(searchText = ""){
+    showLoader();
+    fetch(`https://openapi.programming-hero.com/api/phero-tube/videos?title=${searchText}`)
     .then((res)=>res.json())
-    .then((data)=>displayVideos(data.videos));
+    .then((data)=>{
+        document.getElementById("btn-all").classList.add("active");
+        displayVideos(data.videos)
+
+    });
+    
 }
 
 const loadCategoryVideos=(id)=>{
-    
+    showLoader();
     const url=`
     https://openapi.programming-hero.com/api/phero-tube/category/${id}`;
     console.log(url);
     fetch(url)
     .then(res=>res.json())
-    .then(data=>displayVideos(data.category))
+    .then(data=>{
+        removeActiveClass();
+        const clickButton= document.getElementById(`btn-${id}`);
+        clickButton.classList.add("active")
+
+        displayVideos(data.category)
+    });
+};
+
+
+const loadVideoDetails=(videosId)=>{
+    console.log(videosId);
+    const url=`https://openapi.programming-hero.com/api/phero-tube/video/${videosId}`;
+    fetch(url)
+    .then(res=>res.json())
+    .then(data=>displayVideoDetails(data.video));
 }
 
+const displayVideoDetails=(video)=>{
+    console.log(video)
+    document.getElementById("video_details").showModal();
 
+    const detailsContainer = document.getElementById("details-container");
+    detailsContainer.innerHTML=`
+    <div class="card bg-base-100 image-full  shadow-sm">
+  <figure>
+    <img
+      src="${video.thumbnail}"
+      alt="Shoes" />
+  </figure>
+  <div class="card-body">
+    <h2 class="card-title">${video.authors[0].profile_name}</h2>
+    <p>Building a Winning UX Strategy Using the Kano Model</p>
+    <div class="card-actions justify-end">
+    </div>
+  </div>
+</div>
+    
+    `
+
+}
 
 
 function displayCategories(categories){
@@ -39,7 +100,7 @@ function displayCategories(categories){
         //create element
         const categoryDiv= document.createElement("div");
         categoryDiv.innerHTML=`
-        <button onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
+        <button id="btn-${cat.category_id}" onclick="loadCategoryVideos(${cat.category_id})" class="btn btn-sm hover:bg-[#FF1F3D] hover:text-white">${cat.category}</button>
         `;
         //append the element
         categoryContainer.append(categoryDiv);
@@ -71,6 +132,17 @@ const displayVideos=(videos)=>{
 
     videoContainer.innerHTML="";
 
+    if(videos.length==0){
+        videoContainer.innerHTML=`
+        <div class=" py-20 col-span-full flex flex-col text-current justify-center items-center">
+            <img class="w-[140px]" src="assest/Icon.png" alt="">
+            <h2 class="text-2xl font-bold">Oops!! Sorry, There is no content here</h2>
+        </div>
+        `
+        hideLoader();
+        return;
+    }
+
 
     videos.forEach(video => {
         //element 
@@ -95,17 +167,24 @@ const displayVideos=(videos)=>{
             </div>
             <div class="intro">
                 <h2 class="text-1xl font-semibold">Building a Winning UX Strategy Using the Kano Model</h2>
-                <p class="text-sm text-gray-400 flex gap-1">${video.authors[0].profile_name} <img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">
+                <p class="text-sm text-gray-400 flex gap-1">
+                ${video.authors[0].profile_name}
+                ${video.authors[0].verified==true ? `<img class="w-5 h-5" src="https://img.icons8.com/?size=96&id=98A4yZTt9abw&format=png" alt="">`:``}
                 </p>
                 <p class="text-sm text-gray-400">${video.others.views} views</p>
             </div>
             
             </div>
+            <button onclick="loadVideoDetails('${video.video_id}')" class="btn btn-block">Show Details</button>
         </div>
         `;
         videoContainer.append(videoCard)
     });
+    hideLoader();
 };
+document.getElementById("search-input").addEventListener("keyup",(e)=>{
+    const input=e.target.value;
+    loadVideos(input);
+});
 
-
-loadCategories()
+loadCategories();
